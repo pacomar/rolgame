6ef6a66efc55c400e2c0463b4bfbc9e98c8de247
Francisco Martínez Espartero <Francisco Martnez Espartero>
2016-06-08 12:22:32 +0200

    primer commit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { RolGameAppComponent, environment } from './app/';
+import {FIREBASE_PROVIDERS,
+	defaultFirebase,
+	AngularFire,
+	AuthMethods,
+	AuthProviders,
+	firebaseAuthConfig} from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(RolGameAppComponent);
+bootstrap(RolGameAppComponent, [
+	FIREBASE_PROVIDERS,
+	defaultFirebase('https://rolgame.firebaseio.com/'),
+	firebaseAuthConfig({
+		provider: AuthProviders.Google,
+		method: AuthMethods.Redirect
+	})
+]);
 
